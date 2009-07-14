00ecb9c9413e95683c48b8b5ff7e11653b96e113
Daniel Danopia <me.github@danopia.net>
2009-07-13 21:43:58 -0400

    Licensed under BSD and split into files

---
+++ ircchannel.rb
@@ -0,0 +1,137 @@
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of Danopia nor the names of its contributors may be used
+#   to endorse or promote products derived from this software without specific
+#   prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+class IRCChannel
+	attr_reader :name, :users
+	attr_reader :owners, :protecteds, :ops, :halfops, :voices
+	attr_reader :bans, :invex, :excepts
+	attr_reader :modes, :mode_timestamp
+	attr_reader :topic, :topic_timestamp
+	attr_accessor :topic_author
+	
+	def initialize(name)
+		@name = name
+		@users = []
+		
+		@owners = []
+		@protecteds = []
+		@ops = []
+		@halfops = []
+		@voices = []
+		
+		@bans = []
+		@invex = []
+		@excepts = []
+		
+		@modes = 'ns'
+		@mode_timestamp = Time.now.to_i
+		
+		@topic = nil
+		@topic_author = nil
+		@topic_timestamp = nil
+	end
+
+	def self.find(name)
+		name = name.downcase
+		$server.channels.each do |channel|
+			return channel if channel.name.downcase == name
+		end
+		nil
+	end
+
+	def self.find_or_create(name)
+		channel = IRCChannel.find name
+		return channel if channel
+		
+		channel = IRCChannel.new(name)
+		$server.channels << channel
+		channel
+	end
+	
+	def send_to_all(msg)
+		users.each do |user|
+			user.puts msg
+		end
+	end
+	def send_to_all_except(nontarget, msg)
+		users.each do |user|
+			user.puts msg unless user == nontarget
+		end
+	end
+	
+	def modes=(modes)
+		@modes = modes
+		@modes_timestamp = Time.now
+	end
+	
+	def topic=(topic)
+		@topic = topic
+		@topic_timestamp = Time.now
+	end
+	
+	def message(sender, message)
+		send_to_all_except sender, ":#{sender.path} PRIVMSG #{name} :#{message}"
+	end
+	def notice(sender, message)
+		send_to_all_except sender, ":#{sender.path} NOTICE #{name} :#{message}"
+	end
+	
+	def part(client, message='Leaving')
+		send_to_all ":#{client.path} PART #{name} :#{message}"
+		remove client
+	end
+	
+	def remove(client)
+		@users.delete(client)
+		
+		@owners.delete(client)
+		@protecteds.delete(client)
+		@ops.delete(client)
+		@halfops.delete(client)
+		@voices.delete(client)
+		
+		destroy('Channel empty') if empty?
+	end
+	
+	def empty?
+		@users.empty?
+	end
+	
+	def destroy(reason='OM NOM NOM')
+		@users.each do |user|
+			user.kicked_from self, $server.name, reason
+		end
+		$server.channels.delete self
+	end
+	
+	def has_mode?(mode)
+		@modes.include? mode
+	end
+	def has_any_mode?(modes)
+		modes.split('').each do |mode|
+			return true if has_mode?(mode)
+		end
+		false
+	end
+end
---
+++ ircserver.rb
@@ -0,0 +1,145 @@
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of Danopia nor the names of its contributors may be used
+#   to endorse or promote products derived from this software without specific
+#   prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+# Thanks to Tsion for the Server class
+require 'socket'
+
+class IRCServer
+  attr_accessor :debug, :clients, :channels, :name, :maxConnections, :listeners, :listen_socks, :socks, :running
+  
+	def initialize(name=nil)
+    @debug = true
+    @clients = []
+    @channels = []
+    @name = name
+    @maxConnections = maxConnections
+    @listeners = []
+    @listen_socks = []
+		@socks = []
+		@running = false
+	end
+	
+	def add_listener(host=nil, port=6667)
+		@listeners << [host || '0.0.0.0', port]
+		@listen_socks << TCPServer.new(host || '0.0.0.0', port) if @running
+	end
+	
+  def log(msg)
+    puts "[#{Time.new.ctime}] %s" % msg
+  end
+  def log_nick(nick, msg)
+    log "#{@host}:#{@port} #{nick}\t%s" % msg
+  end
+	
+	def run
+		begin
+			if @listeners.empty?
+				log 'No listeners defined!'
+				return false
+			end
+			
+			# Create TCPServers for each listener
+			@listen_socks.clear
+			@listeners.each do |listener|
+				@listen_socks << TCPServer.new(listener[0], listener[1])
+			end
+			
+			@running = true
+			log("Server started with #{@listen_socks.size} listener" +
+				(@listen_socks.size == 1 ? '' : 's'))
+
+			#BasicSocket.do_not_reverse_lookup = true
+
+			loop do
+				active = nil
+				remove_closed
+				begin
+					active = select(@listen_socks + @socks)[0][0]
+				rescue IOError
+					next # next time will remove closed sockets again
+				end
+				next if not active
+				
+				if @listen_socks.include? active
+					new_sock = active.accept
+					new_client = IRCClient.new new_sock
+					log "connected: #{new_client.ip}" if @debug
+					@socks << new_sock
+					@clients << new_client
+				else
+					if active.eof?
+						active.client.skill
+						@clients.delete(active.client)
+						@socks.delete(active)
+						log "disconnected: #{active.client.ip}" if @debug
+						active.close
+						next
+					end
+					line = active.gets
+					if line == '' or line == nil
+						log 'FAILURE'
+					else
+						begin
+							active.client.handle_packet line
+						rescue => error
+							log error.to_s
+							active.client.skill 'Error occured'
+							@clients.delete(active.client)
+							@socks.delete(active)
+							log "disconnected due to error: #{active.client.ip}"
+						end
+					end
+				end
+			end
+		
+		#rescue => error
+			log error.to_s
+			@socks.each do |sock|
+				sock.client.skill 'Server is going down NOW!'
+			end
+			@socks.clear
+			@listen_socks.each do |listener|
+				listener.close
+			end
+			@listen_socks.clear
+			@running = false
+			p error
+			error.throw
+		end
+	end
+	
+	def remove_closed
+		@socks.reject! do |sock|
+			sock.closed? #|| sock.eof?
+		end
+	end
+	
+	def remove_client(client)
+		remove_sock client.io
+	end
+	def remove_sock(sock)
+		@socks.delete sock
+		@clients.delete sock.client
+	end
+end
