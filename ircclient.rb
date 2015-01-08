d368246d78e6face0efb4418de208ffddaffb185
Daniel Lamando <me@danopia.net>
2015-01-08 11:08:19 -0800

    Fix require statement to be relative
    Ruby 1.9 change

--- ircclient.rb
+++ ircclient.rb
@@ -25,7 +25,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-require 'lineconnection'
+require './lineconnection'
 
 class IRCClient < LineConnection
   attr_accessor :server
